478d06ae73c40ec7e10d48217b05cfe826f7efde
tanimorimizuki <mizuki.tanimori@rakus-partners.co.jp>
2020-09-09 14:50:23 +0900

    first commit

---
+++ src/main/java/com/example9/TabeeApplication.java
@@ -0,0 +1,13 @@
+package com.example9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabeeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example9/domain/User.java
@@ -0,0 +1,5 @@
+package com.example9.domain;
+
+public class User {
+
+}
