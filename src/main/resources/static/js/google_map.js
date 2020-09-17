9811a2cd22357621717a7eaceb953de7d57b66b1
nosemasashi <ma.nose9413@gmail.com>
2020-09-17 15:03:11 +0900

    goggle mapをひとまず表示させました。

--- src/main/resources/static/js/google_map.js
+++ src/main/resources/static/js/google_map.js
@@ -2,14 +2,15 @@
  * 
  */
 $(function() {
-	
+
 	alert('読み込まれてるよ');
 
-	var MyLatLng = new google.maps.LatLng(35.6811673, 139.7670516);
-	var Options = {
-		zoom : 15, // 地図の縮尺値
-		center : MyLatLng, // 地図の中心座標
-		mapTypeId : 'roadmap' // 地図の種類
-	};
-	var map = new google.maps.Map(document.getElementById('map'), Options);
+	function initMap() {
+		var opts = {
+			zoom : 15,
+			center : new google.maps.LatLng(35.709984, 139.810703)
+		};
+		var map = new google.maps.Map(document.getElementById("g-map"), opts);
+	}
+
 });
