00b9c3eb5e7f351d119af9d42ce89da8d1ce5f93
nosemasashi <ma.nose9413@gmail.com>
2020-11-20 13:47:24 +0900

    ユーザー情報更新系のコントローラ・サービス作成

---
+++ src/main/java/com/example/service/UpdateUserService.java
@@ -0,0 +1,16 @@
+package com.example.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * ユーザー情報更新系のサービス
+ * 
+ * @author masashi.nose
+ *
+ */
+@Service
+@Transactional
+public class UpdateUserService {
+
+}
