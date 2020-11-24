401052fbc09b4b57e525018e049c6eea94055fd3
tanimorimizuki <mizuki.tanimori@rakus-partners.co.jp>
2020-11-24 18:31:12 +0900

    フォルダー作成に使うコントローラークラスを作成しました。

--- src/main/java/com/example/controller/scrap/CreateFolderController.java
+++ src/main/java/com/example/controller/scrap/CreateFolderController.java
@@ -1,5 +1,25 @@
 package com.example.controller.scrap;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.domain.scrap.Scrap;
+import com.example.form.scrap.CreateFolderForm;
+import com.example.service.scrap.CreaterFolderService;
+
+@RestController
+@RequestMapping("/scrap")
 public class CreateFolderController {
+	
+	@Autowired
+	private CreaterFolderService createFolderService;
+	
+	@PostMapping("/create_folder")
+	public Scrap createFolder(@RequestBody CreateFolderForm form) {
+		return createFolderService.createFolder(form);
+	}
 
 }
