a3e5019860ec342267892c4db26ab6c1f620c93b
tanimorimizuki <mizuki.tanimori@rakus-partners.co.jp>
2020-11-24 16:28:07 +0900

    スクラップのインサートを行う処理を記述しました。

--- src/main/java/com/example/form/scrap/CreateFolderForm.java
+++ src/main/java/com/example/form/scrap/CreateFolderForm.java
@@ -1,5 +1,8 @@
 package com.example.form.scrap;
 
+import lombok.Data;
+
+@Data
 public class CreateFolderForm {
 
 	/** フォルダー名 */
--- src/main/java/com/example/service/scrap/CreaterFolderService.java
+++ src/main/java/com/example/service/scrap/CreaterFolderService.java
@@ -1,5 +1,8 @@
 package com.example.service.scrap;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,12 +20,41 @@ import com.example.mapper.scrap.ScrapMapper;
 @Service
 @Transactional
 public class CreaterFolderService {
-	
 	@Autowired
 	private ScrapMapper scrapMapper;
 	
+	/**
+	 * フォルダー名を受け取ってscrapテーブルにインサートするメソッド.
+	 * 
+	 * @param form　
+	 * @return インサートされたスクラップ情報
+	 */
 	public Scrap createFolder(CreateFolderForm form) {
-		Scrap scrap = scrapMapper.insertSelective(form);
+		
+		System.out.println("フォルダー名" + form.getFolderName());
+		
+		// スクラップに値をセット
+		Scrap scrap = new Scrap();
+		scrap.setShioriId(1);
+		scrap.setName(form.getFolderName());
+		scrap.setCreatorId(7);
+		scrap.setCreatedAt(new Date());
+		scrap.setVersion(1); 	// 初期装備
+		
+		// 日付の確認用ログ
+		String date = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(scrap.getCreatedAt());
+		System.out.println("登録日" + date);
+		
+		// scrapテーブルにインサート
+		scrapMapper.insertSelective(scrap);
+		
+		// 一件検索
+		Scrap newScrap = new Scrap();
+		newScrap = scrapMapper.selectByPrimaryKey(1);
+		System.out.println(newScrap);
+		
+		return newScrap;
+		
 	}
 
 }
