580213e6bc62d449e1f88ea28309c37efcd40f3c
tanimorimizuki <mizuki.tanimori@rakus-partners.co.jp>
2020-11-24 14:06:07 +0900

    スクラップのファイルをアップロードするサービスクラスを作成しました。

---
+++ src/main/java/com/example/service/scrap/UploadFileService.java
@@ -0,0 +1,16 @@
+package com.example.service.scrap;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * スクラップフォルダーのファイルアップロードサービス.
+ * 
+ * @author mizuki.tanimori
+ *
+ */
+@Service
+@Transactional
+public class UploadFileService {
+
+}
