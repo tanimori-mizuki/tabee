409c9ad7831b4dc28a9da213bfe189251ab7ef1c
okadayuri <chemistory.12@gmail.com>
2020-11-27 18:58:51 +0900

    不要な記述削除

--- src/main/java/com/example/controller/RegisterUserController.java
+++ src/main/java/com/example/controller/RegisterUserController.java
@@ -4,11 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.domain.user.User;
-import com.example.form.LoginForm;
 import com.example.form.RegisterUserForm;
 import com.example.service.RegisterUserSevice;
 
