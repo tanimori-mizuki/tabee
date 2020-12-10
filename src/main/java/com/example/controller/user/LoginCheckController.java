19a57e234468a570c3236b89e5a6b7cf6e0cee64
nosemasashi <ma.nose9413@gmail.com>
2020-12-10 14:27:20 +0900

    gitignoreファイル修正・controllerにuserフォルダ追加

--- src/main/java/com/example/controller/user/LoginCheckController.java
+++ src/main/java/com/example/controller/user/LoginCheckController.java
@@ -1,4 +1,4 @@
-package com.example.controller;
+package com.example.controller.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
--- src/main/java/com/example/controller/user/UpdateUserController.java
+++ src/main/java/com/example/controller/user/UpdateUserController.java
@@ -1,4 +1,4 @@
-package com.example.controller;
+package com.example.controller.user;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
