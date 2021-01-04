b1da04365fd11157447128a5098aa45954a68321
okadayuri <chemistory.12@gmail.com>
2021-01-04 19:21:16 +0900

    OpenWeatherMapAPIで天気予報を取得

---
+++ src/main/java/com/example/common/weather/WeatherApiClient.java
@@ -0,0 +1,27 @@
+package com.example.common.weather;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * API呼び出し情報のクラス.
+ * @author yuri.okada
+ *
+ */
+@Component
+public class WeatherApiClient {
+	
+	private RestTemplate restTemplate=new RestTemplate();
+	
+	/**
+	 * 天気予報を取得する.
+	 * @param city 都市名
+	 * @return　天気予報情報
+	 */
+	public WeatherEntity getWeather(String city) {
+		final String API_KEY="804e576d9cfd447db84a22b1e69f906d";
+		return restTemplate.getForObject("https://api.openweathermap.org/data/2.5/forecast?q="+city+",jp&APPID="+API_KEY+"&lang=ja&units=metric&cnt=20",WeatherEntity.class);
+	}
+
+
+}
---
+++ src/main/java/com/example/controller/shiori/WeatherController.java
@@ -0,0 +1,38 @@
+package com.example.controller.shiori;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.common.weather.WeatherApiClient;
+import com.example.common.weather.WeatherEntity;
+import com.example.common.weather.WeatherResponse;
+
+
+/**
+ * 天気予報を取得するコントロ―ラ.
+ * @author yuri.okada
+ *
+ */
+@RestController
+@RequestMapping("/weather")
+public class WeatherController {
+	
+	@Autowired
+	private WeatherApiClient weatherApiClient;
+	
+	/**
+	 * 天気予報を取得する.
+	 * @param city 都市名
+	 * @return　5日分の天気予報情報
+	 */
+	@GetMapping("")
+	public WeatherResponse getWeather(String city) {
+		WeatherEntity weatherEntity= weatherApiClient.getWeather(city);
+		return new WeatherResponse(weatherEntity.getList());
+	}
+	
+	
+
+}
