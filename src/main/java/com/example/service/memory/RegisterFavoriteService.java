367bbdcd0d16fefda8aaf0881484b178de4d6413
nosemasashi <ma.nose9413@gmail.com>
2021-01-12 18:41:30 +0900

    MemoryMapper内SELECT文を修正

--- src/main/java/com/example/service/memory/RegisterFavoriteService.java
+++ src/main/java/com/example/service/memory/RegisterFavoriteService.java
@@ -1,12 +1,16 @@
 package com.example.service.memory;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.domain.memory.Favorite;
+import com.example.domain.memory.Memory;
 import com.example.form.memory.RegisterFavoriteForm;
 import com.example.mapper.memory.FavoriteMapper;
+import com.example.mapper.memory.MemoryMapper;
 
 /**
  * いいねするサービス.
@@ -21,6 +25,9 @@ public class RegisterFavoriteService {
 	@Autowired
 	private FavoriteMapper favoriteMapper;
 	
+	@Autowired
+	private MemoryMapper memoryMapper;
+	
 	/**
 	 * いいね登録.
 	 * 
@@ -31,6 +38,13 @@ public class RegisterFavoriteService {
 		favorite.setMemoryId(Integer.parseInt(form.getMemoryId()));
 		favorite.setUserId(Integer.parseInt(form.getUserId()));
 		favoriteMapper.insert(favorite);
+		
+		List<Memory> memoryList = memoryMapper.selectAll();
+		for(Memory memory: memoryList) {
+			System.out.println(memory.getId() + ": " + memory.getFavoriteList().size());
+		}
+		
+		
 	}
 }
 
